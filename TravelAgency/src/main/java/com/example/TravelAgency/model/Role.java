af4da799cc3db3bbcaae77b1395c5b9ca40b4064
stevan002 <stevanstankovic603@gmail.com>
2024-04-23 22:25:51 +0200

    work on spring security, generate token

---
+++ TravelAgency/src/main/java/com/example/TravelAgency/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.TravelAgency.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ TravelAgency/src/main/java/com/example/TravelAgency/service/UserDetailsImp.java
@@ -0,0 +1,21 @@
+package com.example.TravelAgency.service;
+
+import com.example.TravelAgency.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsImp implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
